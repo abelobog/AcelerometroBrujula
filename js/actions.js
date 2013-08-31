07ef27d2fe2ce1ebad0ed34293ce2e4c96c7ba62
abelobog <abelobog@hotmail.com>
2013-08-31 12:17:02 -0500

    3

--- js/actions.js
+++ js/actions.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     
 });*/
 //Es lo mismo que la linea anterior
-(function(){
+$(function(){
     document.addEventListener("deviceready",function(){
         $('ul.individual li').tap(function(){accion($(this));
         //alert($(this).parent('page').attr('id'))
@@ -30,32 +30,36 @@ function accion(obj){
 
 var watchID = null;
 function iniciar(p){
-    if (p=='acelerometro')
-        watchID = navigator.accelerometer.watchAcceleration(function(acceleration) {
-    $('#'+p+' h2').html('X: '+ acceleration.x + '<br>'+
-                        'Y: '+ acceleration.y +'<br>'+
-                        'Z: '+ acceleration.z);
-    }, function(){
-        alert('Error en el acelerómetro');
-    }, {frequency: 500});
-    else if (p=='brujula')
-        watchID = navigator.compass.watchHeading(function(heading) {
-            $('#'+p+' h2').html('Magnética: '+ heading.magneticHeading + '<br>'+
-                        'Digital: '+ heading.trueHeading +'<br>'+
-                        'Desviación actual: '+ heading.headingAccuracy);
-        }, function(compassError) {
-            alert('Error en la brujula: ' + compassError.code);
-        }, {frequency: 500});   
-    
+    if (p=='acelerometro'){
+            watchID = navigator.accelerometer.watchAcceleration(function(acceleration) {
+        $('#'+p+' h2').html('X: '+ acceleration.x + '<br>'+
+                            'Y: '+ acceleration.y +'<br>'+
+                            'Z: '+ acceleration.z);
+        }, function(){
+            alert('Error en el acelerómetro');
+        }, {frequency: 500});
+    }
+    else { 
+        if (p=='brujula') {
+            watchID = navigator.compass.watchHeading(function(heading) {
+                $('#'+p+' h2').html('Magnética: '+ heading.magneticHeading + '<br>'+
+                            'Digital: '+ heading.trueHeading +'<br>'+
+                            'Precisión: '+ heading.headingAccuracy);
+            }, function(compassError) {
+                alert('Error en la brujula: ' + compassError.code);
+            }, {frequency: 500});
+        }
+    }
 }
 
 function detener(p){
     if (watchID){
-        if (p=='acelerometro')
+        if (p=='acelerometro') {
             navigator.accelerometer.clearWatch(watchID);
-        else if (p=='brujula')
-            navigator.compass.clearWatch(watchID);
-        
+        } else { 
+            if (p=='brujula')
+                navigator.compass.clearWatch(watchID);
+        }
         watchID = null;
         $('#'+p+' h2').html('Detenido'); 
     }          
